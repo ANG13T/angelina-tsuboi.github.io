e662f3ae01c81656294f58cad13bcdebcc18074a
angelina-tsuboi <angelina.t1832@gmail.com>
2020-01-16 21:28:37 -0800

    Added periods(row numbers)

--- public/script.js
+++ public/script.js
@@ -12,20 +12,18 @@ let periodArry = Array.from(periodicElem);
 for(let i = 0; i < periodArry.length; i++)
 {
     periodArry[i].addEventListener("click", (event)=>{
-        console.log("Clicked", event.target);
         console.log("First Child", event.target.childNodes[1])
         let child = event.target.childNodes[1];
         child.style.backgroundColor = "red";
-        let textWeWant = child.querySelector(".at_num").innerHTML;
-        let otherTextWeWant = child.querySelector(".symbol").innerHTML;
+        let atomicNumber = child.querySelector(".at_num").innerHTML;
+        let symbol = child.querySelector(".symbol").innerHTML;
 
+        //modal stuff
         let modText = document.getElementById("modalText");
-        
-
-        modText.innerHTML= textWeWant;
+        modText.innerHTML= atomicNumber;
 
         let atomText = document.getElementById("atomStuff");
-        atomText.innerHTML = otherTextWeWant;
+        atomText.innerHTML = symbol;
         
     })
     // data-toggle="modal" data-target="#elementModal"
@@ -33,3 +31,8 @@ for(let i = 0; i < periodArry.length; i++)
     periodArry[i].setAttribute("data-target", "#elementModal");
 }
 
+let periodicRows = document.getElementsByClassName("periodic-row");
+
+for(let i = 0; i < periodicRows.length - 3; i++){
+    periodicRows[i].setAttribute("period", i + 1)
+}
