b45d32888766dace8edf845625d0a688ace2d17d
angelina-tsuboi <angelina.t1832@gmail.com>
2020-01-18 08:32:05 -0800

    Added P/N/E functionality

--- public/script.js
+++ public/script.js
@@ -15,15 +15,34 @@ for(let i = 0; i < periodArry.length; i++)
         console.log("First Child", event.target.childNodes[1])
         let child = event.target.childNodes[1];
         child.style.backgroundColor = "red";
+        //variables
         let atomicNumber = child.querySelector(".at_num").innerHTML;
+        let elementName = child.querySelector(".at_details").innerHTML.match(/\w+/);
         let symbol = child.querySelector(".symbol").innerHTML;
+        let atomicMass = child.querySelector(".at_details").innerHTML.match(/\d+\W\d+/);
+        // let period = child.querySelector("")
 
         //modal stuff
-        let modText = document.getElementById("modalText");
-        modText.innerHTML= atomicNumber;
+        let elementSymbolText = document.getElementById("elementSymbol");
+        elementSymbolText.innerHTML= "Symbol: " + symbol;
 
-        let atomText = document.getElementById("atomStuff");
-        atomText.innerHTML = symbol;
+        let atomicNumberText = document.getElementById("atomicNumber");
+        atomicNumberText.innerHTML = "Atomic Number: " + atomicNumber;
+
+        let elementNameText = document.getElementById("elementName");
+        elementNameText.innerHTML = "Element Name: " + elementName.toString().toUpperCase();
+
+        let atomicMassText = document.getElementById("atomicMass");
+        atomicMassText.innerHTML = "Atomic Mass: " + atomicMass;
+
+        let protonsText = document.getElementById("protons");
+        protonsText.innerHTML = "Protons: " + atomicNumber;
+
+        let neutronsText = document.getElementById("neutrons");
+        neutronsText.innerHTML = "Neutrons: " + Math.round(atomicMass - atomicNumber);
+
+        let electronsText = document.getElementById("electrons");
+        electronsText.innerHTML = "Electrons: ";
         
     })
     // data-toggle="modal" data-target="#elementModal"
@@ -33,6 +52,7 @@ for(let i = 0; i < periodArry.length; i++)
 
 let periodicRows = document.getElementsByClassName("periodic-row");
 
+//setting the period for every element
 for(let i = 0; i < periodicRows.length - 3; i++){
     periodicRows[i].setAttribute("period", i + 1)
 }
